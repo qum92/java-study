dad314dc4da65de3590a6da8be59d2b900c54e7a
Qum92 <Oscar9992@naver.com>
2019-03-15 16:18:05 +0900

    2019.03.15

--- src/Practice/Test.java
+++ src/Practice/Test.java
@@ -2,5 +2,19 @@ package Practice;
 
 public class Test {
 	
-
+	public static void main(String[] args) {
+		int[] a = {7,2,9,5,3};
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<a.length;j++) {
+				if(a[i]<a[j]) {
+					int tmp = a[j];
+					a[j] = a[i];
+					a[i] = tmp;
+				}
+			}
+		}
+		for(int i:a) {
+			System.out.print(i);
+		}
+	}
 }
